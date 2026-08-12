e1c12215f3975cb1d6c7bd686405fe3758d22dae
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-08-13 03:17:47 +0600

    chore: update database connection logic in prisma client and server

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL!,
-});
+const { Pool } = pg;
+const connectionString = process.env.DATABASE_URL!;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
 	adapter,
